ef6b75619cd6bc21b1005a578e1118e8633df1bb
Brandon Mayer <b.mayer1@gmail.com>
2014-07-31 16:44:09 -0400

    added reverse lookup from standard name to nc-file name.

--- sciwms/util/cf.py
+++ sciwms/util/cf.py
@@ -17,11 +17,21 @@ def get_by_standard_name(nc, standard_name):
             return v
     return None
 
+def nc_name_from_standard(nc, standard_name):
+    """
+    Reverse lookup from standard name to nc name.
+    """
+    ret = None
+    for k, v in nc.variables.iteritems():
+        if standard_name == v.__dict__.get('standard_name'):
+            ret = k
+            break
+    return ret
+            
 def get_global_attribute(nc, attr):
     try:
         ret = nc.attr
     except:
         ret = None
-    
     return ret
 
