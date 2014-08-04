c22cd95a251b9a64fa54764b4dd8c0278e0e092c
Brandon Mayer <b.mayer1@gmail.com>
2014-08-04 09:54:54 -0400

    finished remove_preliminary

--- sciwms/apps/remove_preliminary.py
+++ sciwms/apps/remove_preliminary.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import multiprocessing
 import os
@@ -5,6 +6,7 @@ import re
 import sys
 import traceback
 import time
+
 
 from owslib import fes, csw
 from netCDF4 import Dataset as ncDataset
@@ -24,13 +26,25 @@ from sciwms.util.cf import get_by_standard_name, nc_name_from_standard, get_glob
 import json
 import numpy as np
 
+output_path = os.path.join(settings.PROJECT_ROOT, 'logs', 'sciwms_wms.log')
+logger = multiprocessing.get_logger()
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(output_path)
+formatter = logging.Formatter(fmt='[%(asctime)s] - <<%(levelname)s>> - |%(message)s|')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def remove_preliminary():
-    
     for dataset in dbDataset.objects.all():
-        print dataset.name
         if 'prelim' in dataset.name or 'preliminary' in dataset.name:
-            print "\t{0}".format(dataset.name)
+            logger.info("Removing {0} from db".format(dataset.name))
+            
+            cache_file_list = glob.glob(os.path.join(settings.TOPOLOGY_PATH,dataset.name + '*'))
+            for cache_file in cache_file_list:
+                logger.info("Removing {0} from topology cache".format(cache_file))
+                os.remove(cache_file)
+                
+        
 
 
 if __name__ == "__main__":
